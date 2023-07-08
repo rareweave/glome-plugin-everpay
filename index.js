b5597a58bc79f1e7daea5c63978271f9df16ee60
Angrymouse <rykov.nikas@gmail.com>
2023-07-08 04:11:13 +0300

    add amount

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ module.exports={
     setup:async()=>{
         const everpay = new Everpay()
         return {
+            id:"everpay",
             async readTxById(id){
                 let dataFromEverpay = await everpay.txByHash(id)
                 if (!dataFromEverpay || dataFromEverpay.action != "transfer" || dataFromEverpay.internalStatus !='success') {
@@ -12,6 +13,7 @@ module.exports={
                 return {
                     coin: dataFromEverpay.tokenSymbol,
                     timestamp: dataFromEverpay.timestamp,
+                    amount:dataFromEverpay.amount,
                     from: dataFromEverpay.from,
                     to:dataFromEverpay.to,
                 }
