b5c01d84cb0d71246dc842254c88d37d0e7377b4
Angrymouse <rykov.nikas@gmail.com>
2023-07-08 05:58:35 +0300

    fix import

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Everpay = require('everpay')
 
 module.exports={
     setup:async()=>{
-        const everpay = new Everpay()
+        const everpay = Everpay.default?new Everpay.default():new Everpay()
         return {
             id:"everpay",
             async readTxById(id){
